357936857ae9d4826482dd715374bf0757ec477e
mcst12345 <herobrinetlb@gmail.com>
2022-07-15 18:13:14 +0800

    commit

--- src/main/java/miku/chaosloli/ChaosLoli.java
+++ src/main/java/miku/chaosloli/ChaosLoli.java
@@ -1,14 +1,10 @@
 package miku.chaosloli;
 
-import net.minecraft.block.Block;
-import net.minecraft.item.Item;
-import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @Mod(
         modid = ChaosLoli.MOD_ID,
@@ -19,11 +15,10 @@ public class ChaosLoli {
     public static final String MOD_ID = "chaosloli";
     public static final String MOD_NAME = "ChaosLoli";
     public static final String VERSION = "1.0-pre1";
-    @Mod.Instance(MOD_ID)
-    public static ChaosLoli INSTANCE;
+
     @Mod.EventHandler
     public void preinit(FMLPreInitializationEvent event) {
-
+        MinecraftForge.EVENT_BUS.register(new Loader());
     }
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
